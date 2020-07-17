7300f82bc3ced288de0201b4ba909d575b868b08
Marwa64 <farabi.marwa@gmail.com>
2020-07-18 00:35:40 +0200

    sending messages

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-let socket;
+let socket, room, thisRole, opponentRole, debatee1, debatee2;
 
 function loadChat() {
   var xhttp = new XMLHttpRequest();
@@ -6,7 +6,37 @@ function loadChat() {
     if (this.readyState == 4 && this.status == 200) {
       document.querySelector(".main").innerHTML = this.responseText;
       socket = io();
-      console.log("clickity click click");
+
+      socket.on('role', data => {
+        console.log(data.room);
+        room = data.room;
+        if (data.role === "red"){
+          thisRole = "#f99999";
+          opponentRole = "#9bc7f1";
+        } else if (data.role === "blue"){
+          thisRole = "#9bc7f1";
+          opponentRole = "#f99999";
+        } else {
+          thisRole = data.role;
+          debatee1 = "#f99999";
+          debatee2 = "#9bc7f1";
+          let messageInput = document.body.querySelector(".messageInput");
+          messageInput.innerText = "You are the judge";
+          messageInput.classList.add("judge");
+        }
+      });
+      socket.on('newMessage', data => {
+        if (thisRole === "judge"){
+          judgeMessages(data.role, data.message);
+        } else {
+          if (data.role === "red" && thisRole != "#f99999"){
+            createMessage(data.message, false);
+          }
+          if (data.role === "blue" && thisRole != "#9bc7f1"){
+            createMessage(data.message, false);
+          }
+        }
+      });
       let sendBtn = document.querySelector("#sendBtn");
       // If send is clicked
       sendBtn.addEventListener('click', () => {
@@ -14,14 +44,13 @@ function loadChat() {
       });
       // If enter is pressed
       window.addEventListener('keydown', ()=>{
-        if (event.keyCode === 13 ){
-          createMessage();
+        try{
+          if (event.keyCode === 13 ){
+            createMessage();
+          }
+        } catch(err){
+          console.log("you're the judge");
         }
-      })
-      // For testing purposes if chat box is clicked
-      let messagesBox = document.querySelector(".messagesBox");
-      messagesBox.addEventListener('click', () => {
-        createMessage("Boooo", false);
       });
     }
   };
@@ -29,18 +58,40 @@ function loadChat() {
   xhttp.send();
 }
 
-function createMessage(text=document.querySelector("input").value, mine=true){
+function createMessage(text=document.querySelector("input").value, mine=true) {
   if (text != ""){
     let message = document.createElement("div");
+    message.innerText = text;
     message.classList.add("message");
     if (mine){
       message.classList.add("mr-auto");
+      message.style.background = thisRole;
+      document.querySelector("input").value="";
+      if (thisRole === "#f99999"){
+        socket.emit('message', {room: room, role: "red", message: text});
+      } else if (thisRole === "#9bc7f1"){
+        socket.emit('message', {room: room, role: "blue", message: text});
+      }
     } else {
       message.classList.add("ml-auto");
+      message.style.background = opponentRole;
     }
-    message.innerText = text;
     document.querySelector(".messagesBox").appendChild(message);
     message.scrollIntoView();
-    document.querySelector("input").value="";
   }
+}
+
+function judgeMessages(color, text) {
+  let message = document.createElement("div");
+  message.classList.add("message");
+  if (color === "red"){
+    message.classList.add("mr-auto");
+    message.style.background = debatee1;
+  } else {
+    message.classList.add("ml-auto");
+    message.style.background = debatee2;
+  }
+  message.innerText = text;
+  document.querySelector(".messagesBox").appendChild(message);
+  message.scrollIntoView();
 }
--- server.js
+++ server.js
@@ -9,9 +9,30 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname+'/index.html');
 })
 
+let roomNum = 0, clientNum = 0, role="";
 io.on('connection', socket => {
-  console.log("A user connected");
+  console.log("A user connected " + clientNum);
+  // We're inceasing the room number if there are 3 people in the room
+  if(io.nsps['/'].adapter.rooms["room-"+roomNum] && io.nsps['/'].adapter.rooms["room-"+roomNum].length == 3){
+    roomNum++;
+    clientNum = 0;
+    console.log("New room!");
+  }
+  socket.join("room-" + roomNum);
+  if (clientNum === 0){
+    role = "judge";
+  } else if (clientNum === 1){
+    role = "red";
+  } else if (clientNum === 2){
+    role = "blue";
+  }
+  clientNum++;
+  socket.emit('role', {role: role, room: roomNum});
 
+  socket.on('message', data => {
+    console.log(data.role);
+    io.sockets.in("room-"+data.room).emit('newMessage', {role: data.role, message: data.message});
+  });
   socket.on('disconnect', () => {
     console.log("A user disconnected");
   })
