a0b6aec7310883ebdee9d7c77be615f5d696d1dc
Marwa64 <farabi.marwa@gmail.com>
2020-07-18 23:21:36 +0200

    pass and join new debate

--- public/js/main.js
+++ public/js/main.js
@@ -5,112 +5,140 @@ function loadChat() {
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       document.querySelector(".main").innerHTML = this.responseText;
-      socket = io();
-
-      let messageInput = document.body.querySelector(".messageInput");
-      messageInput.innerText = "Waiting for debaters to join..";
-      messageInput.classList.add("judge");
-
-      socket.on('role', data => {
-        console.log(data.room);
-        room = data.room;
-        if (data.role === "Red"){
-          thisRole = "#f99999";
-          opponentRole = "#9bc7f1";
-        } else if (data.role === "Blue"){
-          thisRole = "#9bc7f1";
-          opponentRole = "#f99999";
-        } else {
-          thisRole = data.role;
-          debatee1 = "#f99999";
-          debatee2 = "#9bc7f1";
-        }
-      });
-
-      socket.on('start', () => {
-        let messageInput = document.body.querySelector(".messageInput");
-        if (thisRole != "judge"){
-          messageInput.innerHTML = "<div id='messageRow' class='row'><div class='col-9'><input type='text' class='form-control' placeholder='Enter your message'></div><div class='col-3'><button type='button' class='btn btn-warning btn-block btnFont' id='sendBtn'>Send</button></div></div>";
-          let sendBtn = document.querySelector("#sendBtn");
-          sendBtn.addEventListener('click', () => {
-            createMessage();
-          });
-          window.addEventListener('keydown', ()=>{
-            try{
-              if (event.keyCode === 13 ){
-                createMessage();
-              }
-            } catch(err){
-              console.log("Can't send");
-            }
-          });
-        } else {
-          messageInput.innerText = "You are the judge";
-          messageInput.classList.add("judge");
-        }
-        setInterval(checkTyping, 100);
-      });
-
-      socket.on('newMessage', data => {
-        if (thisRole === "judge"){
-          judgeMessages(data.role, data.message);
-        } else {
-          if (data.role === "Red" && thisRole != "#f99999"){
-            createMessage(data.message, false);
-          }
-          if (data.role === "Blue" && thisRole != "#9bc7f1"){
-            createMessage(data.message, false);
-          }
-        }
-      });
-      socket.on('playerLeave', data => {
-        let messageInput = document.body.querySelector(".messageInput");
-        messageInput.innerText = "A player left, this debate is over.";
-        messageInput.classList.add("judge");
-        disconnect();
-      });
-
-      socket.on('playerTyping', data => {
-        let typingBox = document.body.querySelector(".typing");
-        if (thisRole === "judge"){
-          if (typingBox.innerText === ""){
-            typingBox.innerText = data.role + " is typing..";
-          }
-        } else {
-          if (data.role === "Red" && thisRole != "#f99999"){
-            typingBox.innerText = data.role + " is typing..";
-          }
-          if (data.role === "Blue" && thisRole != "#9bc7f1"){
-            typingBox.innerText = data.role + " is typing..";
-          }
-        }
-      });
-
-      socket.on('playerNotTyping', data => {
-        let typingBox = document.body.querySelector(".typing");
-        if (thisRole === "judge"){
-          let newTyping = data.role + " is typing..";
-          if (newTyping === typingBox.innerText){
-            typingBox.innerText = "";
-          }
-        } else {
-          if (data.role === "Red" && thisRole != "#f99999"){
-            typingBox.innerText = "";
-          }
-          if (data.role === "Blue" && thisRole != "#9bc7f1"){
-            typingBox.innerText = "";
-          }
-        }
-      });
-
-      let passBtn = document.body.querySelector(".passBtn");
-      passBtn.addEventListener('click', () => {
-        disconnect();
-      });
+      let passBtn = document.body.querySelector("#passBtn");
+      passBtn.addEventListener('click', pass);
+      joinRoom();
     }
   };
   xhttp.open("GET", "chatroom.html", true);
   xhttp.send();
+}
+
+function joinRoom(){
+  socket = io();
+
+  thisRole = "";
+
+  let messageInput = document.body.querySelector(".messageInput");
+  messageInput.innerText = "Waiting for debaters to join..";
+  messageInput.classList.add("judge");
+
+  socket.on('role', data => {
+    console.log(data.room);
+    room = data.room;
+    if (data.role === "Red"){
+      thisRole = "#f99999";
+      opponentRole = "#9bc7f1";
+    } else if (data.role === "Blue"){
+      thisRole = "#9bc7f1";
+      opponentRole = "#f99999";
+    } else {
+      thisRole = data.role;
+      debatee1 = "#f99999";
+      debatee2 = "#9bc7f1";
+    }
+  });
+
+  socket.on('start', () => {
+    let messageInput = document.body.querySelector(".messageInput");
+    if (thisRole != "judge"){
+      messageInput.innerHTML = "<div id='messageRow' class='row'><div class='col-9'><input type='text' class='form-control' placeholder='Enter your message'></div><div class='col-3'><button type='button' class='btn btn-warning btn-block btnFont' id='sendBtn'>Send</button></div></div>";
+      let sendBtn = document.querySelector("#sendBtn");
+      sendBtn.addEventListener('click', () => {
+        createMessage();
+      });
+      window.addEventListener('keydown', ()=>{
+        try{
+          if (event.keyCode === 13 ){
+            createMessage();
+          }
+        } catch(err){
+          console.log("Can't send");
+        }
+      });
+    } else {
+      messageInput.innerText = "You are the judge";
+      messageInput.classList.add("judge");
+    }
+
+    setInterval(checkTyping, 100);
+  });
+
+  socket.on('newMessage', data => {
+    if (thisRole === "judge"){
+      judgeMessages(data.role, data.message);
+    } else {
+      if (data.role === "Red" && thisRole != "#f99999"){
+        createMessage(data.message, false);
+      }
+      if (data.role === "Blue" && thisRole != "#9bc7f1"){
+        createMessage(data.message, false);
+      }
+    }
+  });
+  socket.on('playerLeave', data => {
+    thisRole = "";
+    disconnect();
+    socket.disconnect();
+  });
+
+  socket.on('playerTyping', data => {
+    let typingBox = document.body.querySelector(".typing");
+    if (thisRole === "judge"){
+      if (typingBox.innerText === ""){
+        typingBox.innerText = data.role + " is typing..";
+      }
+    } else {
+      if (data.role === "Red" && thisRole != "#f99999"){
+        typingBox.innerText = data.role + " is typing..";
+      }
+      if (data.role === "Blue" && thisRole != "#9bc7f1"){
+        typingBox.innerText = data.role + " is typing..";
+      }
+    }
+  });
+
+  socket.on('playerNotTyping', data => {
+    let typingBox = document.body.querySelector(".typing");
+    if (thisRole === "judge"){
+      let newTyping = data.role + " is typing..";
+      if (newTyping === typingBox.innerText){
+        typingBox.innerText = "";
+      }
+    } else {
+      if (data.role === "Red" && thisRole != "#f99999"){
+        typingBox.innerText = "";
+      }
+      if (data.role === "Blue" && thisRole != "#9bc7f1"){
+        typingBox.innerText = "";
+      }
+    }
+  });
+
+}
+
+function pass(){
+  if (thisRole === "#f99999"){
+    socket.emit('pass', {room: room, role: "Red"});
+  } else  if (thisRole === "#9bc7f1") {
+    socket.emit('pass', {room: room, role: "Blue"});
+  } else {
+    socket.emit('pass', {room: room, role: "judge"});
+  }
+}
+
+function join(){
+  console.log("Join!");
+  let passBtn = document.body.querySelector("#passBtn");
+  passBtn.addEventListener('click', pass);
+  passBtn.classList.remove("unactiveBtn");
+  passBtn.classList.add("activeBtn");
+
+  let joinBtn = document.body.querySelector("#joinBtn");
+  joinBtn.removeEventListener('click', join);
+  joinBtn.classList.remove("activeBtn");
+  joinBtn.classList.add("unactiveBtn");
+  joinRoom();
 }
 
 function createMessage(text=document.querySelector("input").value, mine=true) {
@@ -153,17 +181,27 @@ function judgeMessages(color, text) {
 
 function disconnect() {
   console.log("pass");
-  if (thisRole === "#f99999"){
-    socket.emit('pass', {room: room, role: "Red"});
-  } else  if (thisRole === "#9bc7f1") {
-    socket.emit('pass', {room: room, role: "Blue"});
-  } else {
-    socket.emit('pass', {room: room, role: "judge"});
-  }
+  // Activate Join debate button
+  let joinBtn = document.body.querySelector("#joinBtn");
+  joinBtn.addEventListener('click', join);
+  joinBtn.classList.remove("unactiveBtn");
+  joinBtn.classList.add("activeBtn");
+  // Deactivate pass button
+  let passBtn = document.body.querySelector("#passBtn");
+  passBtn.removeEventListener('click', pass);
+  passBtn.classList.remove("activeBtn");
+  passBtn.classList.add("unactiveBtn");
+  // Clear messages
+  let box = document.body.querySelector(".messagesBox");
+  box.innerHTML = "";
+  // Declare that the debate is over
+  let messageInput = document.body.querySelector(".messageInput");
+  messageInput.innerText = "A player left, this debate is over.";
+  messageInput.classList.add("judge");
 }
 
 function checkTyping(){
-  if (thisRole != "judge"){
+  if (thisRole != "judge" && thisRole != ""){
     if (document.querySelector("input").value != ""){
       if (thisRole === "#f99999"){
         socket.emit('typing', {room: room, role: "Red"});
--- server.js
+++ server.js
@@ -9,37 +9,95 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname+'/index.html');
 })
 
-let roomNum = 0, clientNum = 0, role="";
-io.on('connection', socket => {
-  console.log("A user connected " + clientNum);
+let rooms=[], role="", index, newRoom;
 
-  if (clientNum > 2){
-    roomNum++;
-    clientNum = 0;
-    console.log("New room!");
+io.on('connection', socket => {
+  console.log("A user connected " + socket.id);
+
+  if (rooms.length === 0){
+    console.log("new room because there are no rooms")
+    newRoom = {id: rooms.length,
+              client1ID: "",
+              client2ID: "",
+              client3ID: "",
+              start: false};
+    rooms.push(newRoom);
   }
-  if (clientNum === 0){
+  index = rooms.length-1;
+
+  // if this room hasn't started yet, we'll look for the available position and add the new user to it giving them a role as well
+  if (rooms[index].start === false){
+
+    if (rooms[index].client1ID === ""){
+      rooms[index].client1ID = socket.id;
+      role = "judge";
+      socket.join("room-" + rooms[index].id);
+      socket.emit('role', {role: role, room: rooms[index].id});
+
+    } else if (rooms[index].client2ID === ""){
+      rooms[index].client2ID = socket.id;
+      role = "Blue";
+      socket.join("room-" + rooms[index].id);
+      socket.emit('role', {role: role, room: rooms[index].id});
+
+    } else {
+      rooms[index].client3ID = socket.id;
+      role = "Red";
+      socket.join("room-" + rooms[index].id);
+      socket.emit('role', {role: role, room: rooms[index].id});
+    }
+    console.log("Room ID: " + rooms[index].id);
+    console.log("1st: " + rooms[index].client1ID);
+    console.log("2nd: " + rooms[index].client2ID);
+    console.log("3rd: " + rooms[index].client3ID);
+    // If there are 3 users in the room, we'll mark start as true
+    if (rooms[index].client1ID != "" && rooms[index].client2ID != "" && rooms[index].client3ID != ""){
+      console.log("room " + rooms[index].id + " is full, it's starting!");
+      rooms[index].start = true;
+      io.sockets.in("room-"+rooms[index].id).emit('start', "");
+    }
+  } else {
+    console.log("new room because there are no rooms that didn't start");
+    newRoom = {id: rooms.length,
+              client1ID: socket.id,
+              client2ID: "",
+              client3ID: "",
+              start: false};
+    rooms.push(newRoom);
     role = "judge";
-    clientNum++;
-  } else if (clientNum === 1){
-    role = "Red";
-    clientNum++;
-  } else if (clientNum === 2){
-    role = "Blue";
-    clientNum++;
-    io.sockets.in("room-"+roomNum).emit('start', "");
-    socket.emit('start', "");
-  } 
-  socket.join("room-" + roomNum);
-  socket.emit('role', {role: role, room: roomNum});
+    socket.join("room-" + newRoom.id);
+    socket.emit('role', {role: role, room: newRoom.id});
+  }
 
   socket.on('message', data => {
     io.sockets.in("room-"+data.room).emit('newMessage', {role: data.role, message: data.message});
   });
 
   socket.on('pass', data => {
-    io.sockets.in("room-"+data.room).emit('playerLeave', {role: data.role});
     socket.leave("room-" + data.room);
+    socket.emit('playerLeave', {role: data.role});
+    for (let i = 0; i < rooms.length; i++){
+      if (rooms[i].id === data.room){
+        // If a user passed before the debate started, we'll just remove their id from the room and wait for another user
+        if (rooms[i].start === false){
+          if (rooms[i].client1ID === socket.id){
+            rooms[i].client1ID = "";
+          } else if (rooms[i].client2ID === socket.id){
+            rooms[i].client2ID = "";
+          } else {
+            rooms[i].client3ID = "";
+          }
+          // If a user passed while the debate was taking place, we'll inform all the users the player left and end the debate
+        } else {
+          io.sockets.in("room-"+data.room).emit('playerLeave', {role: data.role});
+          for (let i = 0; i < rooms.length; i++){
+            if (rooms[i].id === data.room){
+              rooms.splice(i,1);
+            }
+          }
+        }
+      }
+    }
   });
 
   socket.on('typing', data => {
